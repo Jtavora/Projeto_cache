c162377a11d95cd9e2d1310cd6ec14bfca0c20fa
jtavora <jtavora09@gmail.com>
2024-11-30 17:40:35 -0300

    Implementado boas praticas

--- E-Commerce/Controllers/ProdutosController.cs
+++ E-Commerce/Controllers/ProdutosController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using E_Commerce.Context;
+using E_Commerce.Interfaces;
 using E_Commerce.Models;
 
 namespace E_Commerce.Controllers
