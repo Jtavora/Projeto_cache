282e122f06f97e77786be3171e06c3dcbb3059d7
jtavora <jtavora09@gmail.com>
2024-12-02 00:14:09 -0300

    Ok

---
+++ script.sql
@@ -0,0 +1,17 @@
+INSERT INTO Produtos (Nome, Descricao, Preco, QuantidadeEstoque, Ativo, DataCriacao, DataAtualizacao)
+VALUES 
+('Cafeteira Eletrônica', 'Cafeteira com capacidade para 12 xícaras', 129.99, 50, 1, GETDATE(), GETDATE()),
+('Fone de Ouvido Bluetooth', 'Fone de ouvido sem fio com cancelamento de ruído', 199.99, 80, 1, GETDATE(), GETDATE()),
+('Cadeira Ergonomica', 'Cadeira com ajuste de altura e apoio lombar', 349.99, 30, 1, GETDATE(), GETDATE()),
+('Monitor LED 24"', 'Monitor com resolução Full HD', 799.99, 20, 1, GETDATE(), GETDATE()),
+('Teclado Mecânico', 'Teclado RGB com switches mecânicos', 249.99, 40, 1, GETDATE(), GETDATE()),
+('Smartphone Android', 'Smartphone com 128GB de armazenamento', 999.99, 60, 1, GETDATE(), GETDATE()),
+('Mesa de Escritório', 'Mesa espaçosa com gavetas', 499.99, 15, 1, GETDATE(), GETDATE()),
+('Fritadeira Sem Óleo', 'Fritadeira elétrica de 4 litros', 229.99, 25, 1, GETDATE(), GETDATE()),
+('Mochila para Notebook', 'Mochila resistente à água com compartimento para notebook', 99.99, 70, 1, GETDATE(), GETDATE()),
+('Ventilador de Mesa', 'Ventilador silencioso com três velocidades', 79.99, 50, 1, GETDATE(), GETDATE()),
+('Impressora Multifuncional', 'Impressora com função de scanner e cópia', 499.99, 10, 1, GETDATE(), GETDATE()),
+('Câmera Digital', 'Câmera com resolução de 20MP e zoom óptico', 899.99, 12, 1, GETDATE(), GETDATE()),
+('Relógio Smartwatch', 'Relógio com monitoramento de atividades físicas', 349.99, 35, 1, GETDATE(), GETDATE()),
+('Bicicleta de Montanha', 'Bicicleta com 21 marchas e suspensão dupla', 1199.99, 5, 1, GETDATE(), GETDATE()),
+('Kindle Paperwhite', 'Leitor de e-books com tela antirreflexo', 499.99, 18, 1, GETDATE(), GETDATE());
